0d80447aea11a3fe93b49ee89a8297ee267dd3cb
LiyuanSilviaZhao <zhao0301liyuan@gmail.com>
2022-12-04 23:37:17 -0800

    adjusting project

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Card from "react-bootstrap/Card";
 
 function Project(props) {
 	const currentProject = useState(props)[0].projects;
@@ -8,28 +7,23 @@ function Project(props) {
 	const image = currentProject.image;
 	const appLink = currentProject.deployed;
 	const gitLink = currentProject. github;
-
-	return (
-		<Card style={{ width: "18rem" }}>
-			<Card.Img
-				variant="top"
-				src={require(`../../assets/images/${image}`)}
-				className="card-image"
-			/>
-			<div>
-				<Card.Body>
-					<Card.Title className="card-title">{name}</Card.Title>
-					<Card.Link href={appLink} target="_blank" className="card-link">
-						{name} App link
-					</Card.Link>
-					<br></br>
-					<Card.Link href={gitLink} target="_blank" className="card-link">
-						{name} Github link
-					</Card.Link>
-				</Card.Body>
+return (
+	<div className="card">
+		<img
+			variant="top"
+			src={require(`../../assets/images/${image}`)}
+			className="card-image"
+		/>
+			<div className="card-content">
+				<h4 className="card-title">{name}</h4>
+				<a href={appLink} target="_blank" className="card-link">
+					{name} App link
+				</a>
+				<a href={gitLink} target="_blank" className="card-link">
+					& {name} Github link
+				</a>
 			</div>
-		</Card>
-	);
+	</div>
+);
 }
-
 export default Project;
